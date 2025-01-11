bbb9961481df8a03b5059b7f012d369365c84265
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-01-11 21:15:40 +0700

    Initial source

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header">
+      
+      </div>
+
+      <div className="main">
+        <div className='left-box'>
+
+        </div>
+        <div className='right-box'>
+
+        </div>
+      </div>
+      
+      <div className="footer">
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
